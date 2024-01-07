7e630e24d0d146b1262c09256b1880bff47c2811
chechna9 <ja_bouchouareb@esi.dz>
2024-01-07 22:34:57 +0100

    bottom navbar done

--- app/src/main/java/com/example/algeriansupremecourt/MainActivity.kt
+++ app/src/main/java/com/example/algeriansupremecourt/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.algeriansupremecourt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.algeriansupremecourt.databinding.ActivityMainBinding
+import com.example.algeriansupremecourt.features.favorites.Favorite
+import com.example.algeriansupremecourt.features.home.Home
+import com.example.algeriansupremecourt.features.settings.Settings
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.fav -> replaceFragment(Favorite())
+                R.id.settings -> replaceFragment(Settings())
+
+                else ->{
+                }
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
